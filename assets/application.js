033d58716ff78b18f44d7b9d78cb7bf072f9e097
Tunghsiao Liu <info@sparanoid.com>
2012-09-07 22:17:36 +0800

    Fixed FOUT issue again, use white version on iOS (webkit filter not implemented yet)

--- assets/application.js
+++ assets/application.js
@@ -1,10 +1,11 @@
+// animation
 $(window).load(function() {
   // hide the pins
   $("body").addClass("in");
   $("#spinner").removeClass("in");
 });
 
-// enable :active
+// enable :active, easy method
 document.addEventListener("touchstart", function(){}, true);
 
 // .js spinner
@@ -16,3 +17,15 @@ Spinner({
   // shadow: true,
   hwaccel: false
 }).spin(document.getElementById('spinner'));
+
+// https://gist.github.com/1190492
+function hideAddressBar() {
+  if(!window.location.hash) {
+    if(document.height < window.outerHeight) {
+      document.body.style.height = (window.outerHeight + 50) + 'px';
+    }
+    setTimeout( function(){ window.scrollTo(0, 1); }, 50 );
+  }
+}
+window.addEventListener("load", function(){ if(!window.pageYOffset){ hideAddressBar(); } } );
+window.addEventListener("orientationchange", hideAddressBar );
